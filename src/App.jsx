af6069001091891e02fecb939365fbaf369dd4a4
Rifate nur shawn <33421686+Rifate-nur-shawn@users.noreply.github.com>
2025-07-28 16:08:51 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
+gsap.registerPlugin(ScrollTrigger)
+
+function App() {
+  return (
+    <main>
+        <div>
+            <h1 className="text-3xl font-bold text-pink  underline">Welcome to the world of gta 6 and gsap</h1>
+            
+        </div>
+    </main>
+  )
+}
+
+export default App
