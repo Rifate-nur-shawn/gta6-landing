935a9af197aa75587cc1a6bdc0192ce4affbc332
Rifate nur shawn <33421686+Rifate-nur-shawn@users.noreply.github.com>
2025-08-01 12:25:13 +0600

    Add Jason component and integrate it into App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,21 @@ import React from "react";
 import gsap from "gsap";
 import Hero from "./sections/Hero";
 import NavBar from "./sections/NavBar";
-
-
 import { ScrollTrigger } from "gsap/all";
 import FristVideo from "./sections/FristVideo";
+import Jason from "./sections/Jason";
 
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
   return (
     <main>
-        <NavBar />
-        <Hero />
-        <FristVideo />
+      <NavBar />
+      <Hero />
+      <FristVideo />
+      <Jason />
     </main>
-  ); 
+  );
 }
 
 export default App;
